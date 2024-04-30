001ffce2490acd351bd692548be63492c09d23d4
Yoseph Gebremedhin <yosephbet@gmail.com>
2024-04-30 12:59:39 +0000

    feat: Set DEBUG to False on codestar/settings.py file

--- codestar/codestar/settings.py
+++ codestar/codestar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6bo8btaww8bd5h9784i(nrc53u=8s=b)mmf^axmx629-=3ih+$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-yosephdev-djangoblog-dze6izeu23k.ws-eu110.gitpod.io']
 
