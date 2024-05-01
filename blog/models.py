e4386685b5095331dfb507a34514f4cde33a078e
Yoseph Gebremedhin <yosephbet@gmail.com>
2024-05-01 09:10:03 +0000

    Add slug attribute to Post model in models.py

--- blog/models.py
+++ blog/models.py
@@ -3,4 +3,5 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=200, unique=True) 
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
