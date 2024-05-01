e984e34ffdcc1aaa2062cc519b1904380ef5aeb3
Yoseph Gebremedhin <yosephbet@gmail.com>
2024-05-01 09:49:44 +0000

    Change the value of DEBUG back to False

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-yosephdev-djangoblog-dze6izeu23k.ws-eu110.gitpod.io', '.herokuapp.com']
 
