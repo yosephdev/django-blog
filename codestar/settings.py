faace7c1a100ed94358c275cdf0660a013cf1b8c
Yoseph Gebremedhin <yosephbet@gmail.com>
2024-05-01 09:44:31 +0000

    Add trusted origins for CSRF protection

--- codestar/settings.py
+++ codestar/settings.py
@@ -130,3 +130,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.gitpod.io",
+    "https://*.herokuapp.com"
+]
