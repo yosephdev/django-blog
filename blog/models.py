ef4e4623111f7a70461d1eab5a1ffd6bbd459468
Yoseph Gebremedhin <yosephbet@gmail.com>
2024-05-01 09:11:48 +0000

    Add author attribute to Post model in models.py

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,6 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+    User, on_delete=models.CASCADE, related_name="blog_posts"
+)
