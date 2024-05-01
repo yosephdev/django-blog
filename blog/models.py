c04e1b6b624ce00f45c99bae1bfe3ca58258e0ef
Yoseph Gebremedhin <yosephbet@gmail.com>
2024-05-01 09:15:05 +0000

    Add content attribute to Post model in models.py

--- blog/models.py
+++ blog/models.py
@@ -6,5 +6,5 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-    User, on_delete=models.CASCADE, related_name="blog_posts"
-)
+    User, on_delete=models.CASCADE, related_name="blog_posts")
+    content = models.TextField()
