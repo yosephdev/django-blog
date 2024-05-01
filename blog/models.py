8e679e6a8385e7ec582d00e1e5f4aa5ea460417a
Yoseph Gebremedhin <yosephbet@gmail.com>
2024-05-01 09:32:22 +0000

    Swap the order of choices for the status field in the Post model

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-status = models.IntegerField(choices=STATUS, default=0)
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
 class Post(models.Model):
@@ -12,3 +12,4 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     STATUS = ((0, "Draft"), (1, "Published"))
+    status = models.IntegerField(choices=STATUS, default=0)
