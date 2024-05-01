5bf8022dcb88b6c56d8d11cb63738f2d10b266f6
Yoseph Gebremedhin <yosephbet@gmail.com>
2024-05-01 09:19:30 +0000

    Add status attribute to Post model in models.py

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+status = models.IntegerField(choices=STATUS, default=0)
+
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
