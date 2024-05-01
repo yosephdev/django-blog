2674fcfd98169d3735b64ab7486a0261c0bdc190
Yoseph Gebremedhin <yosephbet@gmail.com>
2024-05-01 08:06:03 +0000

    Connect to DATABASE_URL environment variable in settings.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -86,6 +86,9 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 #     }
 # }
 
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
