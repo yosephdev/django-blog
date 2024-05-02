f68c4afae38c1361f4e4af3e160408a64fcb0923
yosephdev <yosephbet@gmail.com>
2024-05-02 18:30:29 +0200

    Add TEMPLATES_DIR to list of template directories

--- codestar/settings.py
+++ codestar/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = "codestar.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [TEMPLATES_DIR],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
