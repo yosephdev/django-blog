6f40310fd3b6de776e8860664a9697f1b876fe76
yosephdev <yosephbet@gmail.com>
2024-05-02 10:41:43 +0200

    Delete blog_views import from codestar/urls.py

--- codestar/urls.py
+++ codestar/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
-from blog.views import my_blog
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('blog/', my_blog, name='blog'),
+    path("admin/", admin.site.urls),
+    path("blog/", my_blog, name="blog"),
 ]
