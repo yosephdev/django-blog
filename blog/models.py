36bb6205bbb2faeeeb35342261d92afc6274776d
yosephdev <yosephbet@gmail.com>
2024-05-02 09:21:23 +0200

    Add Comment model metadata and str method

--- blog/models.py
+++ blog/models.py
@@ -31,3 +31,9 @@ class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.body}"
