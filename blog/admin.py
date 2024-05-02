81a37fc7185e1db43e03d390e0a503779d223506
yosephdev <yosephbet@gmail.com>
2024-05-02 17:44:48 +0200

    Append created_on to the list_display attribute on blog/admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -6,7 +6,7 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
-    list_display = ("title", "slug", "status")
+    list_display = ("title", "slug", "status", "created_on")
     search_fields = ["title"]
     list_filter = ("status",)
     prepopulated_fields = {"slug": ("title",)}
