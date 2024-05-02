e5ff3920fafdfa100507ed00e531472e32b7aa68
yosephdev <yosephbet@gmail.com>
2024-05-02 15:59:03 +0200

    Delete the existing Post model registration on blog/admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,7 @@ from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
+@admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
     list_display = ("title", "slug", "status")
@@ -13,5 +14,4 @@ class PostAdmin(SummernoteModelAdmin):
 
 
 # Register your models here.
-admin.site.register(Post)
 admin.site.register(Comment)
