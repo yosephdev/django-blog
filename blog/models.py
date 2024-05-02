27d3df048805e20789371867e88791631f5ad740
yosephdev <yosephbet@gmail.com>
2024-05-02 07:34:20 +0200

    Create Comment model below Post model

--- blog/models.py
+++ blog/models.py
@@ -15,3 +15,12 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now_add=True)
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+    Post, on_delete=models.CASCADE, related_name="comments")
+    body = models.TextField()
+    author = models.ForeignKey(
+    User, on_delete=models.CASCADE, related_name="commenter")
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
