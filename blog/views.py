d9257073a6bc19277ad90f5cc7812375fa16fe2f
yosephdev <yosephbet@gmail.com>
2024-05-02 11:30:18 +0200

    Update queryset to order posts by created_on in PostList view

--- blog/views.py
+++ blog/views.py
@@ -5,5 +5,5 @@ from .models import Post
 
 # Create your views here.
 class PostList(generic.ListView):
-    queryset = Post.objects.all()
+    queryset = Post.objects.all().order_by("created_on")
     template_name = "post_list.html"
