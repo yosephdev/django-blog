78648b9e07267bdd5b68764a21dfbcbe89d30525
yosephdev <yosephbet@gmail.com>
2024-05-02 11:37:51 +0200

    Filter queryset in PostList view to display only published posts

--- blog/views.py
+++ blog/views.py
@@ -5,5 +5,5 @@ from .models import Post
 
 # Create your views here.
 class PostList(generic.ListView):
-    queryset = Post.objects.all().order_by("created_on")
+    queryset = Post.objects.filter(status=1)
     template_name = "post_list.html"
