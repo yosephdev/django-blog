be7aec89dcc83635aa02a2ae32794a0eb6831b88
yosephdev <yosephbet@gmail.com>
2024-05-03 11:33:11 +0200

    Order posts by creation date and set context_object_name to 'post_list'

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from .models import Post
 
 # Create your views here.
 class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1)
+    queryset = Post.objects.filter(status=1).order_by("-created_on")
     template_name = "blog/index.html"
     paginate_by = 6
+    context_object_name = "post_list"
