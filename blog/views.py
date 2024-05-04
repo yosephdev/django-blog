d55ea35f760c1391e8397157a9693ed0ad9dcb4e
yosephdev <yosephbet@gmail.com>
2024-05-04 14:40:19 +0200

    Add a post_detail function-based view inside blog/views.py

--- blog/views.py
+++ blog/views.py
@@ -9,3 +9,27 @@ class PostList(generic.ListView):
     template_name = "blog/index.html"
     paginate_by = 6
     context_object_name = "post_list"
+
+
+def post_detail(request, slug):
+    """
+    Display an individual :model:`blog.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+
+    **Template:**
+
+    :template:`blog/post_detail.html`
+    """
+
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "blog/post_detail.html",
+        {"post": post},
+    )
