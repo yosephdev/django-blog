fba1d6e51795bf2a91f69c03dde1f0a3649d4100
yosephdev <yosephbet@gmail.com>
2024-05-04 16:09:44 +0200

    Add serving of static files in DEBUG mode to urls.py

--- codestar/urls.py
+++ codestar/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -24,3 +26,6 @@ urlpatterns = [
     path("summernote/", include("django_summernote.urls")),
     path("", include("blog.urls"), name="blog-urls"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
