d7ef161eccd890e02e8a11491d5b90dd5474dcbc
yosephdev <yosephbet@gmail.com>
2024-05-05 10:41:37 +0200

    Create 'about' views and import the model to about/views.py

--- about/views.py
+++ about/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import About
+
 
 # Create your views here.
+def about_me(request):
+    """
+    Renders the About page
+    """
+
+    about = About.objects.all().order_by("-created_on").first()
+
+    return render(
+        request,
+        "about/about.html",
+        {"about": about},
+    )
