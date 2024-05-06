e778fc43c971d0c5b5da5029316b50d0f85c8ce5
yosephdev <yosephbet@gmail.com>
2024-05-06 19:00:13 +0200

    Add Comment model, reverse and HttpResponseRedirect import

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
 from django.contrib import messages
-from .models import Post
+from django.http import HttpResponseRedirect
+from .models import Post, Comment
 from .forms import CommentForm
 
 
