02dbf8421c6216f93bcec0b02901fcb155b14c99
yosephdev <yosephbet@gmail.com>
2024-05-06 19:04:31 +0200

    Add comment_edit view for editing comments

--- blog/views.py
+++ blog/views.py
@@ -56,3 +56,26 @@ def post_detail(request, slug):
             "comment_form": comment_form,
         },
     )
+
+
+def comment_edit(request, slug, comment_id):
+    """
+    view to edit comments
+    """
+    if request.method == "POST":
+
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comment = get_object_or_404(Comment, pk=comment_id)
+        comment_form = CommentForm(data=request.POST, instance=comment)
+
+        if comment_form.is_valid() and comment.author == request.user:
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.approved = False
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, "Comment Updated!")
+        else:
+            messages.add_message(request, messages.ERROR, "Error updating comment!")
+
+    return HttpResponseRedirect(reverse("post_detail", args=[slug]))
