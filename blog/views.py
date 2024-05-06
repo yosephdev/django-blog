b461f76fe216ec87994ae21397001cbcc6f99ad5
yosephdev <yosephbet@gmail.com>
2024-05-06 10:03:48 +0200

    Add functionality to handle comment submission in the post_detail view

--- blog/views.py
+++ blog/views.py
@@ -31,6 +31,14 @@ def post_detail(request, slug):
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
 
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+    if comment_form.is_valid():
+        comment = comment_form.save(commit=False)
+        comment.author = request.user
+        comment.post = post
+        comment.save()
+
     comment_form = CommentForm()
 
     return render(
