5b6997956a0f43f6784f90911211b0f211f32027
yosephdev <yosephbet@gmail.com>
2024-05-06 10:16:17 +0200

    Add an import to the blog/views.py file for messages

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import Post
 from .forms import CommentForm
 
