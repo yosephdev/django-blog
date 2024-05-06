72fbc99ed6029362ea14f1c28ba64c59392d0918
yosephdev <yosephbet@gmail.com>
2024-05-06 16:45:52 +0200

    Implement collaboration request storage and success message

--- about/views.py
+++ about/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import About
 from .forms import CollaborateForm
+from django.contrib import messages
 
 
 # Create your views here.
@@ -8,6 +9,16 @@ def about_me(request):
     """
     Renders the About page
     """
+
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(
+                request,
+                messages.SUCCESS,
+                "Collaboration request received! I endeavour to respond within 2 working days.",
+            )
 
     about = About.objects.all().order_by("-updated_on").first()
 
