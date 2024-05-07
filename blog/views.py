3a0bce05e6c8af5a8d5cb2bf3cf432c994c75a3e
yosephdev <yosephbet@gmail.com>
2024-05-07 12:31:44 +0200

    Add docstrings to document the purpose of the functions

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,22 @@ from .forms import CommentForm
 
 # Create your views here.
 class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1).order_by("created_on")
+    """
+    Returns all published posts in :model:`blog.Post`
+    and displays them in a page of six posts.
+    **Context**
+
+    ``queryset``
+        All published instances of :model:`blog.Post`
+    ``paginate_by``
+        Number of posts per page.
+
+    **Template:**
+
+    :template:`blog/index.html`
+    """
+
+    queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
     paginate_by = 6
     context_object_name = "post_list"
