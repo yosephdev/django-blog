b4587c7a372085e827cd2926f36c282c01a21284
yosephdev <yosephbet@gmail.com>
2024-05-07 12:35:04 +0200

    Add docstring to document the purpose of the functions

--- blog/views.py
+++ blog/views.py
@@ -75,8 +75,18 @@ def post_detail(request, slug):
 
 def comment_edit(request, slug, comment_id):
     """
-    view to edit comments
+    Display an individual comment for edit.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+    ``comment``
+        A single comment related to the post.
+    ``comment_form``
+        An instance of :form:`blog.CommentForm`
     """
+
     if request.method == "POST":
 
         queryset = Post.objects.filter(status=1)
@@ -98,8 +108,16 @@ def comment_edit(request, slug, comment_id):
 
 def comment_delete(request, slug, comment_id):
     """
-    view to delete comment
+    Delete an individual comment.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+    ``comment``
+        A single comment related to the post.
     """
+
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
     comment = get_object_or_404(Comment, pk=comment_id)
