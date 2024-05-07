797a9f19718f4e81a47dbcb742dd8cfda36230f4
yosephdev <yosephbet@gmail.com>
2024-05-07 12:01:01 +0200

    Reorder import in about/views.py

--- about/views.py
+++ about/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
 from .forms import CollaborateForm
-from django.contrib import messages
 
 
 # Create your views here.
