89dd3068295f3c559e215c754ebd3e2966e08452
yosephdev <yosephbet@gmail.com>
2024-05-07 14:39:18 +0200

    Add tests for required fields in about/test_forms.py

--- about/test_forms.py
+++ about/test_forms.py
@@ -10,3 +10,28 @@ class TestCollaborateForm(TestCase):
             {"name": "john", "email": "test@test.com", "message": "Hello!"}
         )
         self.assertTrue(form.is_valid(), msg="Form is not valid")
+
+    def test_name_is_required(self):
+        """Test for the 'name' field"""
+        form = CollaborateForm(
+            {"name": "", "email": "test@test.com", "message": "Hello!"}
+        )
+        self.assertFalse(
+            form.is_valid(), msg="Name was not provided, but the form is valid"
+        )
+
+    def test_email_is_required(self):
+        """Test for the 'email' field"""
+        form = CollaborateForm({"name": "Matt", "email": "", "message": "Hello!"})
+        self.assertFalse(
+            form.is_valid(), msg="Email was not provided, but the form is valid"
+        )
+
+    def test_message_is_required(self):
+        """Test for the 'message' field"""
+        form = CollaborateForm(
+            {"name": "Matt", "email": "test@test.com", "message": ""}
+        )
+        self.assertFalse(
+            form.is_valid(), msg="Message was not provided, but the form is valid"
+        )
