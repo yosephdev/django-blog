3d6cabf6a4e60bbf0bcdaea9b49da0ee61062be0
yosephdev <yosephbet@gmail.com>
2024-05-07 14:00:59 +0200

    Add an optional msg argument to assertTrue in test_forms.py

--- blog/test_forms.py
+++ blog/test_forms.py
@@ -7,4 +7,4 @@ class TestCommentForm(TestCase):
 
     def test_form_is_valid(self):
         comment_form = CommentForm({"body": "This is a great post"})
-        self.assertTrue(comment_form.is_valid())
+        self.assertTrue(comment_form.is_valid(), msg="Form is not valid")
