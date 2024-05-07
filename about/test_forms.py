d91ba9f6d0ab4da00595728e8dd20edcc271ba65
yosephdev <yosephbet@gmail.com>
2024-05-07 14:19:01 +0200

    Import the CollaborateForm and populates it in about/test_forms.py

---
+++ about/test_forms.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+from .forms import CollaborateForm
+
+
+class TestCollaborateForm(TestCase):
+
+    def test_form_is_valid(self):
+        """Test for all fields"""
+        form = CollaborateForm(
+            {"name": "", "email": "test@test.com", "message": "Hello!"}
+        )
+        self.assertFalse(form.is_valid(), msg="Form is not valid")
