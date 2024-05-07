325b53db96e1fd136b0246a29e00e2e8527ab594
yosephdev <yosephbet@gmail.com>
2024-05-07 11:59:19 +0200

    Reorder import in blog/models.py

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
-from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.db import models
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
