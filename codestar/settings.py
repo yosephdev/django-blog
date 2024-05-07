3df43e56117813c0bbcf9ec5f4f2760f62559b7b
yosephdev <yosephbet@gmail.com>
2024-05-07 07:07:54 +0200

    Add cloudinary_storage and cloudinary to INSTALLED_APPS

--- codestar/settings.py
+++ codestar/settings.py
@@ -49,6 +49,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "cloudinary_storage",
+    "cloudinary",
     "django.contrib.sites",
     "allauth",
     "allauth.account",
